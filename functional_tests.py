8fa167ac64677cdbe1d47907fb008a75a557c423
Daya Adianto <addianto@users.noreply.github.com>
2022-07-19 12:39:40 +0700

    Format code using black

--- functional_tests.py
+++ functional_tests.py
@@ -3,15 +3,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.remote.webelement import WebElement
 from webdriver_manager.firefox import GeckoDriverManager
-import selenium.webdriver.common as Common
 
 browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-browser.get('http://localhost:8000')
+browser.get("http://localhost:8000")
 
-logo: WebElement = browser.find_element(By.CLASS_NAME, 'logo')
+logo: WebElement = browser.find_element(By.CLASS_NAME, "logo")
 
-assert 'Congratulations!' in browser.title
-assert 'django' in logo.text
+assert "Congratulations!" in browser.title
+assert "django" in logo.text
 
 browser.quit()
 
--- sitodo/wsgi.py
+++ sitodo/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sitodo.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sitodo.settings")
 
 application = get_wsgi_application()
