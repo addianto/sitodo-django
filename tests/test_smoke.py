2a627160bf6c6e575619534530d533758ebad264
Daya Adianto <dayaadianto@cs.ui.ac.id>
2022-08-24 23:47:53 +0700

    Maybe switching GitHub Action for downloading Chrome helps?

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -16,11 +16,11 @@ class UISmokeTest(LiveServerTestCase):
         if _is_running_on_ci():
             options.headless = True
             # A quickfix based on https://stackoverflow.com/a/50827853
-            options.add_argument("headless")
-            options.add_argument("no-sandbox")
-            options.add_argument("disable-dev-shm-usage")
-            options.add_argument("disable-extensions")
-            options.add_argument("disable-gpu")
+            # options.add_argument("headless")
+            # options.add_argument("no-sandbox")
+            # options.add_argument("disable-dev-shm-usage")
+            # options.add_argument("disable-extensions")
+            # options.add_argument("disable-gpu")
 
         cls.selenium: WebDriver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install())
