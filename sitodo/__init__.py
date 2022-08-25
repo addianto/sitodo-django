ba854757271960d269302b8d9cf7cf0c41ad07b6
Daya Adianto <dayaadianto@cs.ui.ac.id>
2022-08-25 12:57:25 +0700

    Set DEBUG value from environment variable

---
+++ sitodo/__init__.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+
+def getenv_bool(key: str) -> Optional[bool]:
+    """Get an environment variable as boolean value."""
+    from os import getenv
+
+    VALID_TRUE_VALUES: tuple(str) = ("true", "True", "TRUE")
+    VALID_FALSE_VALUES: tuple(str) = ("false", "False", "FALSE")
+
+    value: Optional[str] = getenv(key)
+
+    if value is None:
+        return None
+
+    if value in VALID_TRUE_VALUES:
+        return True
+
+    if value in VALID_FALSE_VALUES:
+        return False
+
+    return False
--- sitodo/settings.py
+++ sitodo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from os import getenv
 from pathlib import Path
+from . import getenv_bool
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!*o4*9ppr)_3b^l$v%iey&ue%($_oo+o_vc$b9k=o_x*8yu_j8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if getenv_bool("DEBUG") is None else getenv_bool("DEBUG")
 
 ALLOWED_HOSTS = (
     [] if getenv("ALLOWED_HOSTS") == None else getenv("ALLOWED_HOSTS").split(",")
